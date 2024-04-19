e9f0beff38032695b51b4dc50d9c276a5c6124d2
seoyeon <wjstjdus96@naver.com>
2024-04-19 14:24:02 +0900

    feat: 상품 등록 폼 react-hook-form 도입

--- src/components/admin/register/RegisterForm.tsx
+++ src/components/admin/register/RegisterForm.tsx
@@ -1,14 +1,30 @@
-import { useForm } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { TextInput } from "../../common/TextInput";
 import ImageInput from "./ImageInput";
 import TextArea from "../../common/TextArea";
 import { Button } from "../../ui/button";
 
+interface IProductData {
+  productImage: string[];
+  productName: string;
+  productCategory: string;
+  productPrice: number;
+  productQunatity: number;
+  productDescription: string;
+}
+
 const RegisterForm = () => {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit } = useForm<IProductData>();
+
+  const onSubmitHandler: SubmitHandler<IProductData> = (data) => {
+    console.log(data);
+  };
 
   return (
-    <div className="flex flex-col gap-6">
+    <form
+      onSubmit={handleSubmit(onSubmitHandler)}
+      className="flex flex-col gap-6"
+    >
       <ImageInput name="productImage" label="상품이미지" />
       <TextInput name="productName" label="상품 이름" register={register} />
       <TextInput
@@ -25,8 +41,8 @@ const RegisterForm = () => {
         />
       </div>
       <TextArea name="productDescription" label="상품 설명" />
-      <Button>등록</Button>
-    </div>
+      <Button type="submit">등록</Button>
+    </form>
   );
 };
 
