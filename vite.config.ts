4cc282f26ad0e769f4ecab4c744cb0aadcfae5b3
seoyeon <wjstjdus96@naver.com>
2024-05-10 14:55:47 +0900

    refactor: 번들 사이즈 축소

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id: string) {
+          if (id.includes("firebase/auth")) {
+            return "@firebase-auth-vendor";
+          }
+          if (id.includes("react-hook-form") || id.includes("zod")) {
+            return "@form-vendor";
+          }
+        },
+      },
+    },
+  },
+});
