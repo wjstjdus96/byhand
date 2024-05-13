04e78b3ad22cdc5f006f9260fd8d64b8c344c3b8
seoyeon <wjstjdus96@naver.com>
2024-05-13 18:02:49 +0900

    feat: 단위 테스트 환경 초기 설정

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+export default {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "^.+\\.svg$": "jest-svg-transformer",
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+};
