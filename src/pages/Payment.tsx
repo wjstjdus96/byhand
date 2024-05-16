b6d45ceb5ba72973e32cf7f07ed226528c8ca623
seoyeon <wjstjdus96@naver.com>
2024-05-16 22:50:27 +0900

    perf: 스크립트 파일 동작 제어

--- src/pages/Payment.tsx
+++ src/pages/Payment.tsx
@@ -20,25 +20,6 @@ const Payment = () => {
     paymentState,
   });
 
-  // useEffect(() => {
-  //   // 스크립트를 동적으로 생성합니다.
-  //   const script = document.createElement("script");
-  //   script.src = "https://cdn.iamport.kr/v1/iamport.js";
-
-  //   // 스크립트가 로드될 때 실행되는 콜백 함수를 정의합니다.
-  //   script.onload = () => {
-  //     // 스크립트가 로드된 후 실행할 코드를 여기에 작성합니다.
-  //   };
-
-  //   // 생성한 스크립트를 <head> 요소에 추가하여 로드합니다.
-  //   document.head.appendChild(script);
-
-  //   // 컴포넌트가 언마운트 될 때 스크립트를 제거합니다.
-  //   return () => {
-  //     document.head.removeChild(script);
-  //   };
-  // }, []); // 빈 배열을 전달하여 컴포넌트가 마운트될 때 한 번만 실행됩니다.
-
   return (
     <Layout>
       <div className="px-[20vw] py-20 min-h-[75vh]">
